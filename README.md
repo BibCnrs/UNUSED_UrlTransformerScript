cd80d1f617a4b0803df1c4905be6c43cc04aa892
nacimgoura <nacim.goura.dev@gmail.com>
2020-03-20 10:58:45 +0100

    add script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # UrlTransformerScript
-Url transformer script for Safari and Edge
+
+Ce script permet de rediriger la page visité vers la version disponible après connexion
+
+## Prérequis
+
+- Utiliser le navigateur de son choix
+- Télécharger Tampermonkey (gratuit sur Edge, Chrome et Firefox, environ 2.5€ sur Safari), l'ensemble des liens de téléchargement sont présent le [site officiel](https://www.tampermonkey.net/index.php?ext=dhdg)
+
+## Installation
+
+1 - Lancer l'extension  
+2 - Ajouter le contenu du fichier `index.js` à l'extension, pour cela cliquer sur le +  
+![image1](./image/image1.png "image1")  
+3 - Désactiver l'extension (Doit être comme l'image)  
+![image2](./image/image2.png "image2")  
+
+## Utilisation
+
+1 - Se rendre sur le site que l'on veut  
+2 - Activer l'extension en cliquant sur le logo de tampermonkey et en décochant `UrlTransormer` 
+![image5](./image/image5.png "image5")  
+![image3](./image/image3.png "image3")  
+3 - Actualiser la page  
+4 - Une popup doit apparaitre au milieu de la page  
+![image4](./image/image4.png "image4")  
+5 - Choisir son domaine et cliquer sur le bouton `redirect` pour être redirigé vers la version disponible de cette page  
+6 - Désactiver l'extension (pour ne pas avoir la popup qui apparait à chaque page visitée)  
---
+++ index.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name UrlTransformer
+// @version 0.1
+// @description urlTransformer for link
+// @author You
+// @match *://*/*
+// @grant none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('urlTransfomer extension launched');
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css.replace(/;/g, ' !important;');
+        head.appendChild(style);
+    }
+
+    addGlobalStyle(`
+    #modalDomain {
+        position: fixed;
+        right: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        display: hidden;
+        z-index: 1000;
+    }
+    .modalDomain-content {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        padding: 1rem 1.5rem;
+        width: 24rem;
+        border: 2px solid black;
+    }
+    #closeDomain-button {
+        float: right;
+        width: 1.5rem;
+        line-height: 1.5rem;
+        text-align: center;
+        cursor: pointer;
+        border-radius: 0.25rem;
+        background-color: lightgray;
+    }
+    #closeDomain-button:hover {
+        background-color: darkgray;
+    }
+    .buttonDomain {
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
+        -webkit-appearance: button;
+    }
+`);
+
+    $('body').append(`
+<div id="modalDomain">
+    <div class="modalDomain-content">
+        <span id="closeDomain-button">&times;</span>
+        <form id="chooseDomain">
+            <label id="label">Domain : </label>
+                    <select id="domain" required="required">
+                        <option value="insb">insb</option>
+                        <option value="inshs">inshs</option>
+                        <option value="inc">inc</option>
+                        <option value="in2p3">in2p3</option>
+                        <option value="inp">inp</option>
+                        <option value="insmi">insmi</option>
+                        <option value="inee">inee</option>
+                        <option value="insu">insu</option>
+                        <option value="insis">insis</option>
+                        <option value="ins2i">ins2i</option>
+                    </select>
+           <button class="buttonDomain" type="submit">redirect</button>
+       </form>
+    </div>
+</div>
+    `);
+
+    $('#modalDomain').show();
+
+    $('#closeDomain-button').click(function () {
+        $('#modalDomain').hide();
+    });
+
+    $('#chooseDomain').submit(function (event) {
+        window.open(`http://${$('#domain').val()}.bib.cnrs.fr/login?url=${document.URL}`);
+    });
+
+})();
